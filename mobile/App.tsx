ae779a2b96f1332e37c6f2d3acebdfd6c26113f1
Tiago Lopes <tiagoltavares2002@gmail.com>
2022-12-03 21:07:18 -0300

    Criação da tela de apresentação
    - Instalação do Native base e de outras bibliotecas
    - Adição dos ícones da tela e da splash screen
    - Criação da tela inicial do app
    - Ajustes na splash screen
    - Adicionado type para arquivos SVGs

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, NativeBaseProvider, Text, Center, Button, Circle } from 'native-base'
+import HatIcon from './assets/hat_icon.svg'
+import PizzaIcon from './assets/pizza_icon.svg'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>+ Rango</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1} bgColor='#59439C'>
+        <Circle size={200} bg='white' rounded='full'>
+          <PizzaIcon width={150} height={150} />
+        </Circle>
+        <Text color='white' textAlign='center' fontSize='3xl' mt={10} mb={4}>
+          +Rango
+        </Text>
+        <HatIcon width={100} height={100} />
+        <Button 
+          w='3xs'
+          h={16}
+          rounded='2xl'
+          mt={10} 
+          bgColor='white'
+          _pressed={{
+            bg: 'gray.400'
+          }}
+        >
+          <Text color='#59439C' fontWeight='bold' fontSize={20}>Estou com fome</Text>
+        </Button>
+      </Center>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
